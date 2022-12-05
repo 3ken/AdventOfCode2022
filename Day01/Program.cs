13db301c0ef2a356074d3380978ecddf46253063
alexander ekstrand <aekstrand92@gmail.com>
2022-12-05 15:44:44 +0100

    Day One Complete

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var calorieCountPerBackpack = new List<int>();
 
-Console.WriteLine("Hello, World!");
+var currentCalories = 0;
+foreach (var line in File.ReadLines(@"C:\Git\AdventOfCode2022\Day01\Data.txt"))
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        calorieCountPerBackpack.Add(currentCalories);
+        currentCalories = 0;
+    }
+    else
+    {
+        currentCalories += int.Parse(line);
+    }
+}
+
+var topCalorieCount = calorieCountPerBackpack.Max();
+Console.WriteLine("Top calorie count " + topCalorieCount);
+var topThreeCaloriesCountSum = calorieCountPerBackpack.OrderByDescending(x => x).Take(3).Sum();
+Console.WriteLine("Top three calorie count sum: " + topThreeCaloriesCountSum);
+Console.ReadLine();
