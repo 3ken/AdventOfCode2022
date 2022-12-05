f4bc0dbef7ec675f4c73dc5bfce5134819ca36a3
alexander ekstrand <aekstrand92@gmail.com>
2022-12-05 17:02:23 +0100

    Work in progress day 2

--- AdventOfCode2022/Day02/Program.cs
+++ AdventOfCode2022/Day02/Program.cs
@@ -1,3 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+var myChoices = new Dictionary<string, int>
+{
+    { "X", 1 },
+    { "Y", 2 },
+    { "Z", 3 }
+};
+
+var opponentChoices = new List<string> { "A", "B", "C" };
+
+var rountPoint = CalculateRoundPoint("", "");
+
+int CalculateRoundPoint(string opponentChoice, string myChoice)
+{
+    switch (opponentChoice)
+    {
+        case "A":
+            switch (myChoice)
+            {
+                case "X":
+                    return 3 + myChoices["X"];
+                case "Y":
+                    return 6 + myChoices["Y"];
+                case "Z":
+                    return 0 + myChoices["Z"];
+            }
+
+            break;
+        case "B":
+            switch (myChoice)
+            {
+                case "X":
+                    return 0 + myChoices["X"];
+                case "Y":
+                    return 3 + myChoices["Y"];
+                case "Z":
+                    return 6 + myChoices["Z"];
+            }
+
+            break;
+        case "C":
+            switch (myChoice)
+            {
+                case "X":
+                    return 6 + myChoices["X"];
+                case "Y":
+                    return 0 + myChoices["Y"];
+                case "Z":
+                    return 3 + myChoices["Z"];
+            }
+
+            break;
+    }
+
+    return 0;
+}
