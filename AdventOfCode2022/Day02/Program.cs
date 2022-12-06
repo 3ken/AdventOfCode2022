51aa199a7be2715070676525eea55319ab168de5
alexander ekstrand <aekstrand92@gmail.com>
2022-12-06 15:33:38 +0100

    Day2 done

--- AdventOfCode2022/Day02/Program.cs
+++ AdventOfCode2022/Day02/Program.cs
@@ -2,16 +2,19 @@
 
 var myChoices = new Dictionary<string, int> { { "X", 1 }, { "Y", 2 }, { "Z", 3 } };
 var totalPoints = 0;
+var secondTotalPoints = 0;
 foreach (var line in File.ReadLines(@"C:\Git\AdventOfCode2022\AdventOfCode2022\Day02\Data.txt"))
 {
     if (!string.IsNullOrEmpty(line))
     {
         var arguments = line.Split(" ");
         totalPoints += CalculateRoundPoint(arguments[0], arguments[1]);
+        secondTotalPoints += CalculateSecondRoundPoint(arguments[0], arguments[1]);
     }
 }
 
 Console.WriteLine(totalPoints);
+Console.WriteLine(secondTotalPoints);
 Console.ReadLine();
 
 int CalculateRoundPoint(string opponentChoice, string myChoice)
@@ -51,6 +54,50 @@ int CalculateRoundPoint(string opponentChoice, string myChoice)
                     return 0 + myChoices["Y"];
                 case "Z":
                     return 3 + myChoices["Z"];
+            }
+
+            break;
+    }
+
+    return 0;
+}
+int CalculateSecondRoundPoint(string opponentChoice, string myChoice)
+{
+    switch (opponentChoice)
+    {
+        case "A":
+            switch (myChoice)
+            {
+                case "X":
+                    return 0 + myChoices["Z"];
+                case "Y":
+                    return 3 + myChoices["X"];
+                case "Z":
+                    return 6 + myChoices["Y"];
+            }
+
+            break;
+        case "B":
+            switch (myChoice)
+            {
+                case "X":
+                    return 0 + myChoices["X"];
+                case "Y":
+                    return 3 + myChoices["Y"];
+                case "Z":
+                    return 6 + myChoices["Z"];
+            }
+
+            break;
+        case "C":
+            switch (myChoice)
+            {
+                case "X":
+                    return 0 + myChoices["Y"];
+                case "Y":
+                    return 3 + myChoices["Z"];
+                case "Z":
+                    return 6 + myChoices["X"];
             }
 
             break;
