3d2e36d191be45245dc8e0530709ae172902b3ce
alexander ekstrand <aekstrand92@gmail.com>
2022-12-07 11:08:45 +0100

    Day 3 work in progress

--- AdventOfCode2022/Day03/Program.cs
+++ AdventOfCode2022/Day03/Program.cs
@@ -1,22 +1,72 @@
-﻿foreach (var line in File.ReadLines(@"C:\Git\AdventOfCode2022\AdventOfCode2022\Day03\Data.txt"))
+﻿var valueDict = new Dictionary<char, int>
 {
-    var sum = 0;
+    { 'a', 1 },
+    { 'b', 2 },
+    { 'c', 3 },
+    { 'd', 4 },
+    { 'e', 5 },
+    { 'f', 6 },
+    { 'g', 7 },
+    { 'h', 8 },
+    { 'i', 9 },
+    { 'j', 10 },
+    { 'k', 11 },
+    { 'l', 12 },
+    { 'm', 13 },
+    { 'n', 14 },
+    { 'o', 15 },
+    { 'p', 16 },
+    { 'q', 17 },
+    { 'r', 18 },
+    { 's', 19 },
+    { 't', 20 },
+    { 'u', 21 },
+    { 'v', 22 },
+    { 'w', 23 },
+    { 'x', 24 },
+    { 'y', 25 },
+    { 'z', 26 },
+    { 'A', 27 },
+    { 'B', 28 },
+    { 'C', 29 },
+    { 'D', 30 },
+    { 'E', 31 },
+    { 'F', 32 },
+    { 'G', 33 },
+    { 'H', 34 },
+    { 'I', 35 },
+    { 'J', 36 },
+    { 'K', 37 },
+    { 'L', 38 },
+    { 'M', 39 },
+    { 'N', 40 },
+    { 'O', 41 },
+    { 'P', 42 },
+    { 'Q', 43 },
+    { 'R', 44 },
+    { 'S', 45 },
+    { 'T', 46 },
+    { 'U', 47 },
+    { 'V', 48 },
+    { 'W', 49 },
+    { 'X', 50 },
+    { 'Y', 51 },
+    { 'Z', 52 }
+};
+
+var sum = 0;
+
+foreach (var line in File.ReadLines(@"C:\Git\AdventOfCode2022\AdventOfCode2022\Day03\Data.txt"))
+{
     if (!string.IsNullOrEmpty(line))
     {
         var itemsInEachComp = line.Length / 2;
         var comp1 = line[.. itemsInEachComp];
         var comp2 = line[itemsInEachComp ..];
         var charInBoth = comp1.Intersect(comp2).First();
-        sum += GetValue(charInBoth);
+        sum += valueDict[charInBoth];
     }
-    Console.WriteLine(sum);
 }
 
-int GetValue(char x)
-{
-    switch (x)
-    {
-        
-    }
-    return 0;
-}
+Console.WriteLine(sum);
+
