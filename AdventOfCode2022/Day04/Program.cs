a77138e3d66d346a0eee01ae52f546df50afc72e
alexander ekstrand <aekstrand92@gmail.com>
2022-12-07 16:28:33 +0100

    day 4 part 1 done

--- AdventOfCode2022/Day04/Program.cs
+++ AdventOfCode2022/Day04/Program.cs
@@ -1,7 +1,18 @@
-﻿foreach (var line in File.ReadLines(@"C:\Git\AdventOfCode2022\AdventOfCode2022\Day04\Data.txt"))
+﻿var total = 0;
+
+foreach (var line in File.ReadLines(@"C:\Git\AdventOfCode2022\AdventOfCode2022\Day04\Data.txt"))
 {
     if (!string.IsNullOrEmpty(line))
     {
-       
+        var pairs = line.Split(",");
+        var range1 = pairs[0].Split("-");
+        var range2 = pairs[1].Split("-");
+        if (int.Parse(range1[0]) >= int.Parse(range2[0]) && int.Parse(range1[1]) <= int.Parse(range2[1])
+            || int.Parse(range2[0]) >= int.Parse(range1[0]) && int.Parse(range2[1]) <= int.Parse(range1[1]))
+        {
+            total++;
+        }
     }
 }
+
+Console.WriteLine(total);
