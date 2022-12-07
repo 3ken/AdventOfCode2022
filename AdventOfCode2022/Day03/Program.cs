095124ed8849d84f4ebb61228c079b2327a00c0a
alexander ekstrand <aekstrand92@gmail.com>
2022-12-07 09:28:23 +0100

    Day 3 work in progress

--- AdventOfCode2022/Day03/Program.cs
+++ AdventOfCode2022/Day03/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿foreach (var line in File.ReadLines(@"C:\Git\AdventOfCode2022\AdventOfCode2022\Day03\Data.txt"))
+{
+    var sum = 0;
+    if (!string.IsNullOrEmpty(line))
+    {
+        var itemsInEachComp = line.Length / 2;
+        var comp1 = line[.. itemsInEachComp];
+        var comp2 = line[itemsInEachComp ..];
+        var charInBoth = comp1.Intersect(comp2).First();
+        sum += GetValue(charInBoth);
+    }
+    Console.WriteLine(sum);
+}
+
+int GetValue(char x)
+{
+    switch (x)
+    {
+        
+    }
+    return 0;
+}
