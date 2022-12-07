aa16e7c7da04eb01fb60096b72030b3032bd0580
alexander ekstrand <aekstrand92@gmail.com>
2022-12-07 16:39:02 +0100

    day 4 done

--- AdventOfCode2022/Day04/Program.cs
+++ AdventOfCode2022/Day04/Program.cs
@@ -1,4 +1,5 @@
 ﻿var total = 0;
+var totalAny = 0;
 
 foreach (var line in File.ReadLines(@"C:\Git\AdventOfCode2022\AdventOfCode2022\Day04\Data.txt"))
 {
@@ -7,12 +8,20 @@ foreach (var line in File.ReadLines(@"C:\Git\AdventOfCode2022\AdventOfCode2022\D
         var pairs = line.Split(",");
         var range1 = pairs[0].Split("-");
         var range2 = pairs[1].Split("-");
-        if (int.Parse(range1[0]) >= int.Parse(range2[0]) && int.Parse(range1[1]) <= int.Parse(range2[1])
-            || int.Parse(range2[0]) >= int.Parse(range1[0]) && int.Parse(range2[1]) <= int.Parse(range1[1]))
+        if (int.Parse(range1[0]) >= int.Parse(range2[0]) && int.Parse(range1[1]) <= int.Parse(range2[1]) ||
+            int.Parse(range2[0]) >= int.Parse(range1[0]) && int.Parse(range2[1]) <= int.Parse(range1[1]))
         {
             total++;
+        }
+        if (int.Parse(range1[0]) >= int.Parse(range2[0]) && int.Parse(range1[0]) <= int.Parse(range2[1]) ||
+            int.Parse(range1[1]) <= int.Parse(range2[1]) && int.Parse(range1[1]) >= int.Parse(range2[0]) ||
+            int.Parse(range2[0]) >= int.Parse(range1[0]) && int.Parse(range2[0]) <= int.Parse(range1[1]) ||
+            int.Parse(range2[1]) <= int.Parse(range1[1]) && int.Parse(range2[1]) >= int.Parse(range1[0]))
+        {
+            totalAny++;
         }
     }
 }
 
 Console.WriteLine(total);
+Console.WriteLine(totalAny);
