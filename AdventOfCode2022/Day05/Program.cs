85a71b05bd566ba01ead3af1c4772571bc2bbf40
alexander ekstrand <aekstrand92@gmail.com>
2022-12-12 17:04:42 +0100

    Day 5 init

--- AdventOfCode2022/Day05/Program.cs
+++ AdventOfCode2022/Day05/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿var stacks = new List<List<char>>();
+foreach (var line in File.ReadLines(@"C:\Git\AdventOfCode2022\AdventOfCode2022\Day05\Data.txt"))
+{
+    if (!string.IsNullOrEmpty(line) && line.StartsWith(" ", StringComparison.Ordinal) || line.StartsWith("[", StringComparison.Ordinal))
+    {
+        if (line.StartsWith(" 1", StringComparison.Ordinal))
+            break;
+    }
+}
+
