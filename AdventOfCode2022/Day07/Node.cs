3d3466b4eabdf7a25c559952bf143d2058c80c61
alexander ekstrand <aekstrand92@gmail.com>
2022-12-20 10:26:00 +0100

    Day 7 work in progress

---
+++ AdventOfCode2022/Day07/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace Day07;
+
+public class Node
+{
+    public string Name { get; set; } = string.Empty;
+    public int Size { get; set; } = 0;
+    public Node? Parent { get; set; }
+    public List<Node> Branches { get; set; } = new();
+
+    public int GetTotalSize()
+    {
+        return Branches.Sum(b => b.Size);
+    }
+}
--- AdventOfCode2022/Day07/Program.cs
+++ AdventOfCode2022/Day07/Program.cs
@@ -1,1 +1,60 @@
-﻿var file = File.ReadLines(@"C:\Git\AdventOfCode2022\AdventOfCode2022\Day07\Data.txt").ToList();
+﻿using Day07;
+
+var file = File.ReadLines(@"C:\Git\AdventOfCode2022\AdventOfCode2022\Day07\Data.txt").ToList();
+
+var currentPosition = new Node { Name = "/" };
+
+CreateFileTree(file, currentPosition);
+ResetToTopStartPosition(currentPosition);
+
+var totalSize = CalculateTotalSize(currentPosition);
+
+void CreateFileTree(List<string> list, Node node)
+{
+    foreach (var line in list)
+    {
+        var splitLine = line.Split(" ");
+        if (splitLine[0] == "$" && line != "$ cd /")
+        {
+            if (splitLine[1] == "cd")
+            {
+                node = node.Branches.First(b => b.Name == splitLine[2]);
+            }
+
+            if (splitLine[1] == "ls")
+            {
+                if (splitLine[2] == ".." && node.Parent != null)
+                {
+                    node = node.Parent;
+                }
+            }
+        }
+
+        if (splitLine[0] == "dir")
+        {
+            node.Branches.Add(new Node { Name = splitLine[1], Parent = node });
+        }
+
+        if (int.TryParse(splitLine[0], out var fileSize))
+        {
+            node.Branches.Add(new Node { Name = splitLine[1], Parent = node, Size = fileSize });
+        }
+    }
+}
+
+void ResetToTopStartPosition(Node node)
+{
+    while (node.Parent != null)
+    {
+        node = node.Parent;
+    }
+}
+
+int CalculateTotalSize(Node currentPosition)
+{
+    var totalSize = 0;
+    while (currentPosition.GetTotalSize() > 10000)
+    {
+        
+    }
+}
