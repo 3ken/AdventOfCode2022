f6ee068c1ff7a251d9503ac91e1ef1469d056c12
alexander ekstrand <aekstrand92@gmail.com>
2022-12-27 16:33:07 +0100

    Day 7:2 done

--- AdventOfCode2022/Day07/Program.cs
+++ AdventOfCode2022/Day07/Program.cs
@@ -7,9 +7,12 @@ var currentPosition = new Node { Name = "/" };
 CreateFileTree(file, currentPosition);
 ResetToTopStartPosition(currentPosition);
 
-var totalSize = CalculateTotalSize(currentPosition);
+var totalFileSystemSize = currentPosition.GetTotalSize();
+var answerOne = CalculateTotalSizeOfAllDirectoriesWithSizeLessThan100000(currentPosition);
+var answerTwo = GetSizeOfDirectoryToDelete(currentPosition, totalFileSystemSize);
 
-Console.WriteLine(totalSize);
+Console.WriteLine(answerOne);
+Console.WriteLine(answerTwo);
 
 void CreateFileTree(List<string> list, Node node)
 {
@@ -26,19 +29,15 @@ void CreateFileTree(List<string> list, Node node)
             node.Branches.Add(new Node { Name = splitLine[1], Parent = node, Type = "dir" });
         }
 
-        if (splitLine[0] == "$")
+        if (splitLine[0] != "$") continue;
+        if (splitLine[1] != "cd" || line == "$ cd /") continue;
+        if (splitLine[2] == ".." && node.Parent != null)
         {
-            if (splitLine[1] == "cd" && line != "$ cd /")
-            {
-                if (splitLine[2] == ".." && node.Parent != null)
-                {
-                    node = node.Parent;
-                }
-                else
-                {
-                    node = node.Branches.First(b => b.Name == splitLine[2]);
-                }
-            }
+            node = node.Parent;
+        }
+        else
+        {
+            node = node.Branches.First(b => b.Name == splitLine[2]);
         }
     }
 }
@@ -51,19 +50,49 @@ void ResetToTopStartPosition(Node node)
     }
 }
 
-int CalculateTotalSize(Node node)
+int CalculateTotalSizeOfAllDirectoriesWithSizeLessThan100000(Node node)
 {
     var totSize = 0;
     if (node.GetTotalSize() <= 100000)
         totSize += node.GetTotalSize();
 
-    foreach (var branch in node.Branches)
-    {
-        if (branch.Type == "dir")
-        {
-            totSize += CalculateTotalSize(branch);
-        }
-    }
+    totSize += node.Branches.Where(branch => branch.Type == "dir").Sum(CalculateTotalSizeOfAllDirectoriesWithSizeLessThan100000);
 
     return totSize;
+}
+
+int GetSizeOfDirectoryToDelete(Node node, int filesystemSize)
+{
+    var directorySizes = new List<int>();
+    GetAllBranchDirectorySizes(node, directorySizes);
+    return CalculateWhatDirectoryToDelete(directorySizes, filesystemSize);
+}
+
+void GetAllBranchDirectorySizes(Node node, List<int> directorySizes)
+{
+    foreach (var branch in node.Branches.Where(branch => branch.Type == "dir"))
+    {
+        directorySizes.Add(branch.GetTotalSize());
+        GetAllBranchDirectorySizes(branch, directorySizes);
+    }
+}
+
+int CalculateWhatDirectoryToDelete(List<int> directorySizes, int filesystemSize)
+{
+    var bestSizeToDelete = 0;
+    var bestSizeYet = int.MaxValue;
+    var spaceNeeded = 30000000;
+    var totalFileSystem = 70000000;
+    foreach (var directorySize in directorySizes)
+    {
+        var fileSystemSizeAfterDeletion = totalFileSystem - filesystemSize + directorySize;
+        if (fileSystemSizeAfterDeletion < spaceNeeded) continue;
+
+        var diff = fileSystemSizeAfterDeletion - spaceNeeded;
+        if (diff >= bestSizeYet) continue;
+        bestSizeYet = diff;
+        bestSizeToDelete = directorySize;
+    }
+
+    return bestSizeToDelete;
 }
